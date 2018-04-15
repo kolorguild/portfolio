b8d18dd464847aec67ceb2af532c9fbfa442a643
David Nel <david.uberdiablo@gmail.com>
2018-04-15 21:27:25 +0200

    Updated version - mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,15 @@
-const greeting = 'Hello World';
-console.log(greeting);
+// Variables
+const btnParent = document.querySelector('.navbar');
+
+// Get all buttons within container
+const btns = btnParent.getElementsByClassName('btn');
+
+// Loop through all the buttons to add active class
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', function(){
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
